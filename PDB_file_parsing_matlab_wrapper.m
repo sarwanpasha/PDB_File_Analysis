b82cad9b5e2069a939f0c33829bed412b124250b
Sarwan Pasha <sarwanpasha@gmail.com>
2022-12-22 20:45:10 -0500

    Add files via upload

--- PDB_file_parsing_matlab_wrapper.m
+++ PDB_file_parsing_matlab_wrapper.m
@@ -1,6 +1,10 @@
 input_file_path = "/data/hummels/digitalSELEX-2/pdbfiles/";
 input_file_name = "1wwf.ent.pdb";
+
 output_file_path = "/data/hummels/digitalSELEX-2/pdbfiles/Temporary_output/";
 
+%if you want single file including protein and aptamers, run this line and comment the next one
+system("python PDB_Single_File_Parsing.py " + input_file_path + " " + input_file_name + " " + output_file_path);
 
-system("python PDB_File_Parsing_All_Models.py " + input_file_path + " " + input_file_name + " " + output_file_path);
+%if you want two separate files for protein and aptamers, run this line and comment the above one
+system("python PDB_Separate_File_Parsing.py " + input_file_path + " " + input_file_name + " " + output_file_path);
