f0e8b16a01483b0b29d5aa39264dae34bbb74343
Sarwan Pasha <sarwanpasha@gmail.com>
2022-12-22 20:58:51 -0500

    Update PDB_file_parsing_matlab_wrapper.m

--- PDB_file_parsing_matlab_wrapper.m
+++ PDB_file_parsing_matlab_wrapper.m
@@ -1,7 +1,7 @@
-input_file_path = "/data/hummels/digitalSELEX-2/pdbfiles/";
+input_file_path = "Sample_Data/";
 input_file_name = "1wwf.ent.pdb";
 
-output_file_path = "/data/hummels/digitalSELEX-2/pdbfiles/Temporary_output/";
+output_file_path = "Sample_Data/";
 
 %if you want single file including protein and aptamers, run this line and comment the next one
 system("python PDB_Single_File_Parsing.py " + input_file_path + " " + input_file_name + " " + output_file_path);
